a6f61ed9f553330815b7245182cc968268da3661
Lucas Monteiro <lucasribeirom@hotmail.com>
2022-05-31 14:01:34 -0300

    FIX: Corrigi classe Mage

--- src/Archetypes/Mage.ts
+++ src/Archetypes/Mage.ts
@@ -3,7 +3,7 @@ import Archetype from './Archetype';
 
 class Mage extends Archetype {
   private readonly _energy: EnergyType;
-  private static mageInstance: number;
+  private static mageInstance = 0;
 
   constructor(name: string) {
     super(name);
